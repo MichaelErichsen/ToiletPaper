e5920d0b8f380c36f33cd2bfcd32f4dce5a17115
michael <michael.erichsen@xact.dk>
2020-04-14 12:50:04 +0200

    Home fragment xml updated

--- app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeSecondFragment.java
+++ app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeSecondFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import net.myerichsen.toiletpaper.R;
 
@@ -20,8 +20,7 @@ public class HomeSecondFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeSecondViewModel =
-                ViewModelProviders.of(this).get(HomeSecondViewModel.class);
+        homeSecondViewModel = new ViewModelProvider(this).get(HomeSecondViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home_second, container, false);
         final TextView textView = root.findViewById(R.id.text_second_home);
         homeSecondViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
--- app/src/main/java/net/myerichsen/toiletpaper/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/net/myerichsen/toiletpaper/ui/slideshow/SlideshowFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 
 import net.myerichsen.toiletpaper.R;
 
@@ -20,8 +20,7 @@ public class SlideshowFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
+        slideshowViewModel = new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
