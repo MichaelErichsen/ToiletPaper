b256594e1c3d11b62c2e56493ce67f58d05c49b0
michael <michael.erichsen@xact.dk>
2020-04-15 09:41:43 +0200

    Home fragment spinners populated

--- app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeFragment.java
+++ app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeFragment.java
@@ -1,15 +1,21 @@
 package net.myerichsen.toiletpaper.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import net.myerichsen.toiletpaper.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -19,7 +25,49 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        Context context = getContext();
 
+        Spinner layersSpinner = root.findViewById(R.id.layersSpinner);
+        ArrayList<String> layerArrayList = new ArrayList<>();
+        layerArrayList.add("2");
+        layerArrayList.add("3");
+        layerArrayList.add("4");
+        layerArrayList.add("5");
+        ArrayAdapter<String> layerArrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, layerArrayList);
+        layerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        layersSpinner.setAdapter(layerArrayAdapter);
+        layersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+        Spinner suppliersSpinner = root.findViewById(R.id.suppliersSpinner);
+        ArrayList<String> supplierArrayList = new ArrayList<>();
+        // TODO Move to SQLite table
+        supplierArrayList.add("Bilka");
+        supplierArrayList.add("Coop");
+        supplierArrayList.add("REMA 1000");
+        supplierArrayList.add("Spar");
+        ArrayAdapter<String> supplierArrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, supplierArrayList);
+        supplierArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        suppliersSpinner.setAdapter(supplierArrayAdapter);
+        suppliersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 //        final TextView textView = root.findViewById(R.id.text_home);
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
--- app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeViewModel.java
+++ app/src/main/java/net/myerichsen/toiletpaper/ui/home/HomeViewModel.java
@@ -4,6 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+/**
+ * Architecture Components provides ViewModel helper class for the UI controller that is responsible
+ * for preparing data for the UI. ViewModel objects are automatically retained during configuration
+ * changes so that data they hold is immediately available to the next activity or fragment
+ * instance. For example, if you need to display a list of users in your app, make sure to assign
+ * responsibility to acquire and keep the list of users to a ViewModel, instead of an activity or
+ * fragment, as illustrated by the following sample code:
+ */
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
