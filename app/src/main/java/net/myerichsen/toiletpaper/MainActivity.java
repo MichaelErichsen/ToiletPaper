bc175f6e48bba7fdf7632c7cd500db4de1de0968
michael <michael.erichsen@xact.dk>
2020-04-21 10:43:02 +0200

    Settings activity added

--- app/src/main/java/net/myerichsen/toiletpaper/MainActivity.java
+++ app/src/main/java/net/myerichsen/toiletpaper/MainActivity.java
@@ -1,7 +1,9 @@
 package net.myerichsen.toiletpaper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -43,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
