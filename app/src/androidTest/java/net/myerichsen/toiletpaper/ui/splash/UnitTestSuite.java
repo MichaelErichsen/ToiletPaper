a4b72e9bb1d502c011f1ff044137bed757d0ce7a
michael <michael.erichsen@xact.dk>
2020-05-23 13:03:07 +0200

    Espresso Tests. Test suite added

---
+++ app/src/androidTest/java/net/myerichsen/toiletpaper/ui/splash/UnitTestSuite.java
@@ -0,0 +1,20 @@
+package net.myerichsen.toiletpaper.ui.splash;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * Copyright (c) 2020. Michael Erichsen.
+ *
+ * The program is distributed under the terms of the GNU Affero General Public License v3.0
+ */
+
+// Runs all unit tests.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({HomeHelpTest.class,
+        HomeItemNoSearchSeveralResultsTest.class,
+        HomeItemNoSearchSeveralResultsTest.class,
+        CompareTest.class})
+public class UnitTestSuite {
+}
+
